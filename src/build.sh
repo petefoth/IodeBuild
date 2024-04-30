44ee535fe915d0a5ea34266b99de36389b7fd357
Pete Fotheringham <petefoth@e.email>
2024-04-30 14:54:56 +0100

    Fix path of IMAGES directory for 21.0

--- src/build.sh
+++ src/build.sh
@@ -367,7 +367,12 @@ for branch in ${BRANCH_NAME//,/ }; do
             files_to_hash+=( "$build" )
           done
 
-          cd "$source_dir/out/target/product/$codename/obj/PACKAGING/target_files_intermediates/lineage_$codename-target_files/IMAGES/"
+          # Now handle the .img files - where are they?
+          img_dir=$(find "$source_dir/out/target/product/$codename/obj/PACKAGING" -name "IMAGES")
+          if [ -d "$img_dir" ]; then
+            cd "$img_dir"
+          fi
+
           if [ "$ZIP_UP_IMAGES" = true ]; then
             # zipping the .img files
             echo ">> [$(date)] Zipping the .img files" | tee -a "$DEBUG_LOG"
