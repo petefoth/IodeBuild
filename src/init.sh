71357f160fcd331411bfc92d9abf2c995fa16877
Pete Fotheringham <petefoth@e.email>
2024-06-28 20:05:26 +0100

    Add new nfc key

--- src/init.sh
+++ src/init.sh
@@ -37,7 +37,7 @@ git config --global user.email "$USER_MAIL"
 if [ "$SIGN_BUILDS" = true ]; then
   if [ -z "$(ls -A "$KEYS_DIR")" ]; then
     echo ">> [$(date)] SIGN_BUILDS = true but empty \$KEYS_DIR, generating new keys"
-    for c in releasekey platform shared media networkstack sdk_sandbox bluetooth; do
+    for c in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testcert verity; do
       echo ">> [$(date)]  Generating $c..."
       /root/make_key "$KEYS_DIR/$c" "$KEYS_SUBJECT" <<< '' &> /dev/null
     done
