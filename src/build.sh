44fed097ca8d927d0dbe4849c801e234c4ca98b7
Pete Fotheringham <petefoth@e.email>
2024-12-06 17:51:39 +0000

    Don't delete vendor/* after each build

--- src/build.sh
+++ src/build.sh
@@ -470,8 +470,8 @@ for branch in ${BRANCH_NAME//,/ }; do
             cd "$source_dir"
             echo ">> [$(date)] Removing $PWD/out" | tee -a "$DEBUG_LOG"
             rm -rf out || true
-            echo ">> [$(date)] Removing $PWD/vendor" | tee -a "$DEBUG_LOG"
-            rm -rf vendor/* || true
+#            echo ">> [$(date)] Removing $PWD/vendor" | tee -a "$DEBUG_LOG"
+#            rm -rf vendor/* || true
             echo ">> [$(date)] Removing $PWD/.repo/local_manifests/roomservice.xml" | tee -a "$DEBUG_LOG"
             rm -f .repo/local_manifests/roomservice.xml
           fi
